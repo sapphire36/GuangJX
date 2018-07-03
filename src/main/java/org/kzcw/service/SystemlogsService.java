721de51887cecc159fa284d6b831ce1655da0c2a
guozemei <codegzm@google.com>
2018-07-03 12:12:58 +0800

    增加了SystemlogsDao SystemlogsService SystemlogsServiceImpl

---
+++ src/main/java/org/kzcw/service/SystemlogsService.java
@@ -0,0 +1,12 @@
+package org.kzcw.service;
+
+import org.kzcw.core.BaseService;
+import org.kzcw.model.Systemlogs;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+@Component("systemlogsService")
+public interface SystemlogsService extends BaseService<Systemlogs>{
+
+}
